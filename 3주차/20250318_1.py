b5b48f60a0f3ea2ff1a442907fc19787d1063d4c
wonderdh <38901829+wonderdh@users.noreply.github.com>
2025-03-19 01:52:27 +0000

    123

--- 3주차/20250318_1.py
+++ 3주차/20250318_1.py
@@ -8,6 +8,9 @@ if img is None:
     sys.exit('파일이 존재하지 않습니다.')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # BGR 컬러 영상을 명암 영상으로 변환
+gh = cv.calcHist([gray], [0], None, [256], [0,256])
+plt.plot(gh, color='b', linewidth = 1)
+#plt.show()
 
 t, bin_img= cv.threshold(gray, 127, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 
